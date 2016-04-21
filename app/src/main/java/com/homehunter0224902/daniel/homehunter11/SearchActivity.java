28a9412f75b445fd1e4f13fca9e8469e7b1f53e0
dcofosho <dlcohen224@gmail.com>
2016-04-21 18:28:46 -0400

    map

--- app/src/main/java/com/homehunter0224902/daniel/homehunter11/SearchActivity.java
+++ app/src/main/java/com/homehunter0224902/daniel/homehunter11/SearchActivity.java
@@ -1,15 +1,37 @@
 package com.homehunter0224902.daniel.homehunter11;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Daniel on 4/21/2016.
  */
-public class SearchActivity extends Activity {
+public class SearchActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+    private GoogleMap map;
+    SupportMapFragment fm;
     TextView textView2;
     String name, email, salaryString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +42,34 @@ public class SearchActivity extends Activity {
             email=getIntent().getStringExtra("email");
             salaryString=getIntent().getStringExtra("salary");
             textView2.setText(name+"\n"+email+"\n"+salaryString);
+            fm = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map);
+            map = fm.getMap();
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+            map.getUiSettings().setZoomGesturesEnabled(true);
+            map.getUiSettings().setRotateGesturesEnabled(true);
         }catch (Exception e){
             textView2.setText(e.getMessage());
         }
+
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
